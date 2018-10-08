635d92c033b945c1b95b9cc60e6819dcf42fd2ca
gorob <bozmail@gmx.de>
2018-10-09 01:22:44 +0200

    Refactoring PhotobookPicture Klasse
    Auslagerung bestehende
    - Markierung-/Resizing-/Verschieben-/Rotation-Logik in eigene Feature-Klassen
    - Cursor Resizing links/rechts/oben/unten entsprechend gesetzt

--- src/photobook/ResizeFeature.java
+++ src/photobook/ResizeFeature.java
@@ -14,32 +14,64 @@ public class ResizeFeature implements IResizeFeature {
         return region;
     }
 
-    private boolean isVergroesserungsbereichRechteEckeOben(double x, double y) {
-        return isInRectangle(x, y,
+    private boolean isResizeAreaTopRight(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
                 getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
                 getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH);
     }
 
-    private boolean isVergroesserungsbereichRechteEckeUnten(double x, double y) {
-        return isInRectangle(x, y,
+    private boolean isResizeAreaRight(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
+                getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
+                getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH + 1,
+                getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH - 1);
+    }
+
+    private boolean isResizeAreaBottomRight(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
                 getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
                 getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
                 getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA);
     }
 
-    private boolean isVergroesserungsbereichLinkeEckeOben(double x, double y) {
-        return isInRectangle(x, y,
+    private boolean isResizeAreaBottom(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + 1,
+                getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA - 1,
+                getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
+                getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA);
+    }
+
+    private boolean isResizeAreaTopLeft(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH);
     }
 
-    private boolean isVergroesserungsbereichLinkeEckeUnten(double x, double y) {
-        return isInRectangle(x, y,
+    private boolean isResizeAreaTop(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH + 1,
+                getRegion().getWidth() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH - 1,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH);
+    }
+
+    private boolean isResizeAreaLeft(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH + 1,
+                INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH + 1,
+                getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH - 1);
+    }
+
+    private boolean isResizeAreaBottomLeft(double mouseX, double mouseY) {
+        return isInRectangle(mouseX, mouseY,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA,
                 INDENT_FROM_BORDER_TO_RESIZE_AREA + RESIZE_AREA_WIDTH,
                 getRegion().getHeight() - INDENT_FROM_BORDER_TO_RESIZE_AREA - RESIZE_AREA_WIDTH,
@@ -51,14 +83,22 @@ public class ResizeFeature implements IResizeFeature {
     }
 
     public void changeMouseCursor(boolean mousePressed, double mouseX, double mouseY) {
-        if (isVergroesserungsbereichRechteEckeOben(mouseX, mouseY)) {
+        if (isResizeAreaTopRight(mouseX, mouseY)) {
             getRegion().getScene().setCursor(Cursor.NE_RESIZE);
-        } else if (isVergroesserungsbereichRechteEckeUnten(mouseX, mouseY)) {
+        } else if (isResizeAreaBottomRight(mouseX, mouseY)) {
             getRegion().getScene().setCursor(Cursor.SE_RESIZE);
-        } else if (isVergroesserungsbereichLinkeEckeOben(mouseX, mouseY)) {
+        } else if (isResizeAreaTopLeft(mouseX, mouseY)) {
             getRegion().getScene().setCursor(Cursor.NW_RESIZE);
-        } else if (isVergroesserungsbereichLinkeEckeUnten(mouseX, mouseY)) {
+        } else if (isResizeAreaBottomLeft(mouseX, mouseY)) {
             getRegion().getScene().setCursor(Cursor.SW_RESIZE);
+        } else if (isResizeAreaRight(mouseX, mouseY)) {
+            getRegion().getScene().setCursor(Cursor.E_RESIZE);
+        } else if (isResizeAreaBottom(mouseX, mouseY)) {
+            getRegion().getScene().setCursor(Cursor.S_RESIZE);
+        } else if (isResizeAreaLeft(mouseX, mouseY)) {
+            getRegion().getScene().setCursor(Cursor.W_RESIZE);
+        } else if (isResizeAreaTop(mouseX, mouseY)) {
+            getRegion().getScene().setCursor(Cursor.N_RESIZE);
         }
     }
 }
